eb3fc20f49049b2bc34b70b51c379e44651e9f73
Bshisia <shisiabrian7@gmail.com>
2025-03-21 01:57:20 +0300

    feat: create a function to load the scores

--- main.go
+++ main.go
@@ -1,23 +1,52 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "encoding/json"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+    "sync"
 )
 
 type Score struct {
-	Name  string `json:"name"`
-	Score int    `json:"score"`
-	Time  int    `json:"time"`
+    Name  string `json:"name"`
+    Score int    `json:"score"`
+    Time  int    `json:"time"`
+}
+
+var scores []Score
+var mu sync.Mutex
+
+func loadScores() {
+    file, err := os.Open("scores.json")
+    if err != nil {
+        if os.IsNotExist(err) {
+            scores = []Score{}
+            return
+        }
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    bytes, err := ioutil.ReadAll(file)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = json.Unmarshal(bytes, &scores)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
 
 func main() {
+    loadScores()
 
-	http.Handle("/", http.FileServer(http.Dir(".")))
-
-	log.Println("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+   
+    log.Println("Listening on :8080...")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
