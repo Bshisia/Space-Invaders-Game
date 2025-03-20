73efacf543f84fe8ad7d1c8440f56fb66e222cbb
Bshisia <shisiabrian7@gmail.com>
2025-03-21 02:00:47 +0300

    chore; create a funtion to save the scores

--- main.go
+++ main.go
@@ -1,62 +1,71 @@
 package main
 
 import (
-    "encoding/json"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "os"
-    "sync"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sync"
 )
 
 type Score struct {
-    Name  string `json:"name"`
-    Score int    `json:"score"`
-    Time  int    `json:"time"`
+	Name  string `json:"name"`
+	Score int    `json:"score"`
+	Time  int    `json:"time"`
 }
 
 var scores []Score
 var mu sync.Mutex
 
 func loadScores() {
-    file, err := os.Open("scores.json")
-    if err != nil {
-        if os.IsNotExist(err) {
-            scores = []Score{}
-            return
-        }
-        log.Fatal(err)
-    }
-    defer file.Close()
+	file, err := os.Open("scores.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			scores = []Score{}
+			return
+		}
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    bytes, err := ioutil.ReadAll(file)
-    if err != nil {
-        log.Fatal(err)
-    }
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    err = json.Unmarshal(bytes, &scores)
-    if err != nil {
-        log.Fatal(err)
-    }
+	err = json.Unmarshal(bytes, &scores)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
+func saveScores() {
+	bytes, err := json.Marshal(scores)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("scores.json", bytes, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func getScores(w http.ResponseWriter, r *http.Request) {
-    mu.Lock()
-    defer mu.Unlock()
+	mu.Lock()
+	defer mu.Unlock()
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(scores)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(scores)
 }
 
-
 func main() {
-    loadScores()
+	loadScores()
 
- 
-    log.Println("Listening on :8080...")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	log.Println("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
